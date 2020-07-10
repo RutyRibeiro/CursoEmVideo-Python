9e6f54833e802eb34db3dfa337622b9ebe6f10eb
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2020-07-09 23:31:28 -0300

    :+1:

--- Exercícios/Desafio_62.py
+++ Exercícios/Desafio_62.py
@@ -1,9 +1,17 @@
-#le primeiro termo e razao de pa, mostra os 10 primeiros termos usando laço while
-print('\033[36mCalculo de PA!\033[m')
+#le primeiro termo e razao de pa, mostra os 10 primeiros termos, após isso mostra apenas a quantidade de termos recebidos 
+print('\033[36mCálculo de PA!\033[m')
+
 razao=int(input('Digite a Razão:'))
 pt=int(input('Digite o Primeiro Termo: '))
+
 print('Os 10 primeiros termos da PA:')
 i=0
 while i<10:
     print('{}° termo: {}'.format(i+1, pt+i*razao))
+    i+=1
+esc=int(input('Deseja mostrar mais quantos termos? '))
+while esc!=0:
+    for i in range(i,i+esc):
+        print('{}° termo: {}'.format(i+1, pt+i*razao))
+    esc=int(input('Deseja mostrar mais quantos termos? '))
     i+=1
---
+++ Exercícios/Desafio_63.py
@@ -0,0 +1,5 @@
+num=int(input('Digite a quantidade de números'))
+i=0
+ant=0
+while i<num:
+    print({})
