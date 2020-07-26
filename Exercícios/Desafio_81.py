cd30c947125221d75345bc4fe3da01935e5a41c1
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2020-07-26 04:50:09 -0300

    :+1:

--- Exercícios/Desafio_81.py
+++ Exercícios/Desafio_81.py
@@ -1,12 +1,16 @@
+# recebe valores pra uma lista mostra a quantidade de elementos digitados, elementos em dorma decrescente e se o numero 5 faz parte desta lista
 esc='S'
-while esc=='S':
-    num=int(input('Digite um valor: '))
-    if num not in lista:
-        
-        i=(input('Deseja continuar [S/N]:')).upper()
-        while i!='S' and i!='N':
-            print('Digite uma opção valida!')
-            i=(input('Deseja continuar [S/N]:')).upper()
-    else:
-        print('Valor já cadastrado, tente novamente!')
+lista=[]
+while esc=='S':    
+    lista.append(int(input('Digite um número: ')))
+    esc=(input('Deseja continuar [S/N]:')).upper()
+    while esc!='S' and esc!='N':
+        print('Digite uma opção valida!')
+        esc=(input('Deseja continuar [S/N]:')).upper()
     print('\n')
+lista.sort(reverse=True)
+print('O tamanho da lista é: {}\nLista descrescente: {}'.format(len(lista),lista))
+if 5 in lista:
+    print('O número 5 está na lista! ')
+else:
+    print('O número não 5 está na lista! ') 
