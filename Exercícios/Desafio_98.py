f516c3300028fa856097b5607e472e3c49477462
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2020-08-03 09:10:06 -0300

    :+1:

--- Exercícios/Desafio_98.py
+++ Exercícios/Desafio_98.py
@@ -3,10 +3,14 @@ from time import sleep
 
 # função responsável por fazer e imprimir a contagem
 def contador(inicio,fim,passo):
+    # corrige o passo caso a contagem seja regressiva e o passo esteja positivo
+    if fim < inicio and passo>0:
+        passo*=-1
     print('-'*50)
-    print(f'Contando de {inicio} a {fim} de {passo} em {passo}\nContagem:', end='')
-    for i in range(inicio,fim+1,passo):
-        sleep(0.5)
+    print(f'Contando de {inicio} á {fim} de {abs(passo)} em {abs(passo)}\nContagem:', end='')
+    # a estrutura condicional contida na função range faz com que todos os números propostos sejam incluidos na contagem 
+    for i in range(inicio, fim+1 if fim > inicio else fim-1,passo):
+        # sleep(0.5)
         print(i,' ' ,end='')
     print('FIM!\n')
 
