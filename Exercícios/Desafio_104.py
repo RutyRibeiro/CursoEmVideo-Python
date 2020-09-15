a154893dfa70dd8b4df9340302d3a72af7cff312
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2020-09-14 22:45:32 -0300

    exercício 104

---
+++ Exercícios/Desafio_104.py
@@ -0,0 +1,17 @@
+# programa com função que só recebe númeos inteiros
+def leiaInt(num):
+    while True:
+        # testa se a string é numerica caso True transforma em um int
+        if num.isnumeric():
+            num=int(num)
+        # testa se é um numero inteiro 
+        if isinstance(num,int):
+            return num
+            break
+        else:
+            print('Digite um valor válido!')
+            num=input('Digite um número inteiro: ')
+
+# programa principal
+num=leiaInt(input('Digite um número inteiro: '))
+print(f'Você digitou o número: {num}')   
